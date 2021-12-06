de5a4c4d0689526d8698aac3519817bc747a7344
stevessschen <36218346+stevessschen@users.noreply.github.com>
2021-12-06 20:01:57 +0800

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -6,16 +6,16 @@ def BMI(w, h):
 #w = float(input('請輸入體重(KG)？'))
 w = st.number_input('請輸入體重(KG)？')
 h = st.number_input('請輸入身高(M)？')
-st.button('輸入確認')
-
-bmi = BMI(w, h)
-#print('BMI為', bmi)
-st.write('BMI為', bmi)
-if (bmi < 18):
-    st.write('體重過輕')
-elif (bmi < 24):
-    st.write('體重正常')
-elif (bmi < 27):
-    st.write('體重過重')
-else:
-    st.write('體重肥胖')
+confirm_input = st.button('輸入確認')
+if confirm_input:
+    bmi = BMI(w, h)
+    #print('BMI為', bmi)
+    st.write('BMI為', bmi)
+    if (bmi < 18):
+        st.write('體重過輕')
+    elif (bmi < 24):
+        st.write('體重正常')
+    elif (bmi < 27):
+        st.write('體重過重')
+    else:
+        st.write('體重肥胖')
