344d4c4fcbe6e3f7ad4e5e8f01c69f600c5e6637
stevessschen <36218346+stevessschen@users.noreply.github.com>
2021-12-16 19:43:14 +0800

    Update car.py

--- car.py
+++ car.py
@@ -1,10 +1,11 @@
 import streamlit as st
 
 x = st.slider('Select a value')
-
+gas = st.sidebar.slider('油量的資料收集:油箱滿是10格 =>', 1, 10, 8)
 car_simulation = {'gas_warning':2, 'speed_limit':100, 'temp_warning':30, '轉速':12000}
 #gas = int(input('油量的資料收集:油箱滿是10格 =>'))
-gas = st.slider('油量的資料收集:油箱滿是10格 =>', 0, 10, 8)
+
+#gas = st.slider('油量的資料收集:油箱滿是10格 =>', 0, 10, 8)
 
 if gas <= car_simulation.get('gas_warning'):
   st.write('油箱只剩', gas, '格! 準備加油!!')
